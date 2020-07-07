fa2f1c7464cbf2599f2894c926c0140fbcc0e0a5
qianqi <qianqi0614@gmail.com>
2020-07-07 10:16:54 +0800

    rename

--- src/FindNearestNumber.java
+++ src/FindNearestNumber.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class findNearestNumber {
+public class FindNearestNumber {
     public static void main(String[] args) {
         int[] inputNumber = inputNumber();
         int[] nextNumber = new int[0];

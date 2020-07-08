3d7b86a6f0eda7c4fff998637ff94af9066d9f64
qianqi <qianqi0614@gmail.com>
2020-07-08 15:33:37 +0800

    LeetCode链表的中间节点快慢指针求法

--- src/test.java
+++ src/test.java
@@ -1,15 +1,17 @@
 public class test {
     public static void main(String[] args) {
         ListNode l1 = new ListNode(1);
-//        ListNode l2 = new ListNode(2);
-//        ListNode l3 = new ListNode(3);
-//        ListNode l4 = new ListNode(4);
-//        ListNode l5 = new ListNode(5);
-//        l1.next = l2;
-//        l2.next = l3;
-//        l3.next = l4;
-//        l4.next = l5;
-        ListNode res = removeNthFromEnd(l1,1);
+        ListNode l2 = new ListNode(2);
+        ListNode l3 = new ListNode(3);
+        ListNode l4 = new ListNode(4);
+        ListNode l5 = new ListNode(5);
+        ListNode l6 = new ListNode(6);
+        l1.next = l2;
+        l2.next = l3;
+        l3.next = l4;
+        l4.next = l5;
+//        l5.next = l6;
+        ListNode res = middleNode(l1);
         while (res != null) {
             System.out.println(res.val);
             res = res.next;
@@ -25,21 +27,19 @@ public class test {
         ListNode(int x) { val = x; }
     }
 
-    public static ListNode removeNthFromEnd(ListNode head, int n) {
-        ListNode dummy = new ListNode(-1);
-        dummy.next = head;
-        ListNode first = head;
+    public static ListNode middleNode(ListNode head) {
+        ListNode curr = head;
         int len = 0;
-        while (first != null) {
-            len ++;
-            first = first.next;
+        while (curr != null) {
+            curr = curr.next;
+            len++;
         }
-        len -= n;
+        len /= 2;
+        curr = head;
         while (len != 0) {
+            curr = curr.next;
             len--;
-            dummy = dummy.next;
         }
-        dummy.next = dummy.next.next;
-        return dummy.next;
+        return curr;
     }
 }

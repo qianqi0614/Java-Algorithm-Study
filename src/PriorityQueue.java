8a5d29cbe1912025c58a13bed54b4bc1108a0473
qianqi <qianqi0614@gmail.com>
2020-07-08 17:21:41 +0800

    优先队列的实现

---
+++ src/PriorityQueue.java
@@ -0,0 +1,98 @@
+import java.util.Arrays;
+
+public class PriorityQueue {
+    private int[] array;
+    private int size;
+
+    public PriorityQueue() {
+        // 队列的初始长度为32
+        array = new int[32];
+    }
+
+    /**
+     * 入队
+     * @param key 入队元素
+     */
+    public void enQueue(int key) {
+        // 队列长度超出范围，扩容
+        if (size >= array.length) {
+            resize();
+        }
+        array[size++] = key;
+        upAdjust();
+    }
+
+    /**
+     * 出队
+     * @return 出队元素
+     * @throws Exception
+     */
+    public int deQueue() throws Exception {
+        if (size <= 0) {
+            throw new Exception("this queue is empty!");
+        }
+        // 获取栈顶元素
+        int head = array[0];
+        // 让最后一个元素移动到栈顶
+        array[0] =array[--size];
+        downAdjust();
+        return head;
+    }
+
+    /**
+     * 上浮调整
+     */
+    private void upAdjust() {
+        int childIndex = size - 1;
+        int parentIndex = (childIndex - 1) / 2;
+        // temp保存插入的叶子节点值，用于最后的赋值
+        int temp = array[childIndex];
+        while (childIndex > 0 && temp > array[parentIndex]) {
+            // 无需真正交换，单向赋值即可
+            array[childIndex] = array[parentIndex];
+            childIndex = parentIndex;
+            parentIndex = parentIndex / 2;
+        }
+        array[childIndex] = temp;
+    }
+
+    /**
+     * 下沉调整
+     */
+    private void downAdjust() {
+        // temp 保存父节点的值，用于最后的赋值
+        int parentIndex = 0;
+        int temp = array[parentIndex];
+        int childIndex = 1;
+        while (childIndex < size) {
+            if (childIndex + 1 < size && array[childIndex + 1] > array[childIndex]) {
+                childIndex++;
+            }
+            if (temp >= array[childIndex]) {
+                break;
+            }
+            array[parentIndex] = array[childIndex];
+            parentIndex = childIndex;
+            childIndex = 2 * childIndex + 1;
+        }
+        array[parentIndex] = temp;
+    }
+
+    private void resize() {
+        int newSize = this.size * 2;
+        this.array = Arrays.copyOf(this.array, newSize);
+    }
+
+    public static void main(String[] args) throws Exception {
+        PriorityQueue priorityQueue = new PriorityQueue();
+        priorityQueue.enQueue(3);
+        priorityQueue.enQueue(5);
+        priorityQueue.enQueue(10);
+        priorityQueue.enQueue(2);
+        priorityQueue.enQueue(7);
+        System.out.println("出队元素：" + priorityQueue.deQueue());
+        System.out.println("出队元素：" + priorityQueue.deQueue());
+        System.out.println("出队元素：" + priorityQueue.deQueue());
+        System.out.println("出队元素：" + priorityQueue.deQueue());
+    }
+}

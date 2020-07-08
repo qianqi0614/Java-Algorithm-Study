e54983e594256e24f2ac0c7c4978d20466d5dc63
qianqi <qianqi0614@gmail.com>
2020-07-08 14:28:37 +0800

    Leetcode合并两个有序链表题目及其debug

--- src/test.java
+++ src/test.java
@@ -1,109 +1,53 @@
-import java.util.Arrays;
-import java.util.Scanner;
-
 public class test {
     public static void main(String[] args) {
-        int[] inputNumber = inputNumber();
-        try {
-            System.out.print("prevNumber: ");
-            output(findPrevNumber(inputNumber));
-        } catch (NullPointerException e) {
-            System.out.println("Not found!");
-        }
-        try {
-            System.out.println("nextNmuber: ");
-            output(findNextNumber(inputNumber));
-        } catch (NullPointerException e) {
-            System.out.println("Not found!");
+        ListNode l11 = new ListNode(1);
+        ListNode l12 = new ListNode(2);
+        ListNode l13 = new ListNode(3);
+        ListNode l21 = new ListNode(1);
+        ListNode l22 = new ListNode(2);
+        ListNode l23 = new ListNode(4);
+        l11.next = l12;
+        l12.next = l13;
+        l21.next = l22;
+        l22.next = l23;
+        ListNode res = mergeTwoLists(l11,l21);
+        while (res != null) {
+            System.out.println(res.val);
+            res = res.next;
         }
     }
 
-    private static int[] findPrevNumber(int[] inputNumber) {
-        int index = findPrevTransfer(inputNumber);
-        if (index == 0) {
-            return null;
-        }
-        int[] numberCopy = Arrays.copyOf(inputNumber, inputNumber.length);
-        exchangePrev(numberCopy, index);
-        reserve(numberCopy, index);
-        return numberCopy;
+    public static class ListNode {
+        int val;
+        ListNode next;
 
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
     }
 
-    private static void exchangePrev(int[] nums, int index) {
-        int temp = nums[index - 1];
-        for (int i = nums.length - 1; i > 0; i--) {
-            if (nums[i] > temp) {
-                nums[index] = nums[i];
-                nums[i] = temp;
-                break;
+    public static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode prevhead = new ListNode(-1);
+        ListNode prev = prevhead;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                prev.next = l1;
+                l1 = l1.next;
+            } else {
+                prev.next = l2;
+                l2 = l2.next;
             }
+            prev = prev.next;
         }
-    }
-
-    private static int[] findNextNumber(int[] inputNumber) {
-        int index = findNextTransfer(inputNumber);
-        if (index == 0) {
-            return null;
-        }
-        int[] numberCopy = Arrays.copyOf(inputNumber, inputNumber.length);
-        exchangeNext(numberCopy, index);
-        reserve(numberCopy, index);
-        return numberCopy;
-    }
-
-    private static void exchangeNext(int[] nums, int index) {
-        int temp = nums[index - 1];
-        for (int i = nums.length - 1; i > 0; i--) {
-            if (nums[i] < temp) {
-                nums[index] = nums[i];
-                nums[i] = temp;
-                break;
-            }
-        }
-    }
-
-    private static void reserve(int[] nums, int index) {
-        for (int i = index, j = nums.length - 1; i < j; i++,j--) {
-            nums[i] = nums[i] + nums[j];
-            nums[j] = nums[i] - nums[j];
-            nums[i] = nums[i] - nums[j];
-        }
-    }
-
-    private static int findNextTransfer(int[] nums) {
-        for (int i = nums.length - 1; i > 0; i--) {
-            if (nums[i] > nums[i - 1]) {
-                return i;
-            }
-        }
-        return 0;
-    }
-
-    private static int findPrevTransfer(int[] nums) {
-        for (int i = nums.length - 1; i > 0; i--) {
-            if (nums[i] < nums[i - 1]) {
-                return i;
-            }
-        }
-        return 0;
-    }
-
-    private static int[] inputNumber() {
-        Scanner sc = new Scanner(System.in);
-        String input = sc.nextLine();
-        int[] inputNumber = new int[input.length()];
-        for (int i = 0; i < input.length(); i++) {
-            char c = input.charAt(i);
-            inputNumber[i] = Character.getNumericValue(c);
-        }
-        return inputNumber;
-    }
-
-    private static void output(int[] prevNumber) {
-        for (int i : prevNumber) {
-            System.out.print(i);
-        }
-        System.out.println();
+        prev.next = l1 == null ? l2 : l1;
+        return prevhead.next;
     }
 }

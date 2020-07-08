549e6e211fc6d73edbb62116137e4da3e2d78c80
qianqi <qianqi0614@163.com>
2020-07-08 15:13:16 +0800

    leetcode删除链表的倒数第 n 个节点题目”

--- src/test.java
+++ src/test.java
@@ -1,16 +1,15 @@
 public class test {
     public static void main(String[] args) {
-        ListNode l11 = new ListNode(1);
-        ListNode l12 = new ListNode(2);
-        ListNode l13 = new ListNode(3);
-        ListNode l21 = new ListNode(1);
-        ListNode l22 = new ListNode(2);
-        ListNode l23 = new ListNode(4);
-        l11.next = l12;
-        l12.next = l13;
-        l21.next = l22;
-        l22.next = l23;
-        ListNode res = mergeTwoLists(l11,l21);
+        ListNode l1 = new ListNode(1);
+//        ListNode l2 = new ListNode(2);
+//        ListNode l3 = new ListNode(3);
+//        ListNode l4 = new ListNode(4);
+//        ListNode l5 = new ListNode(5);
+//        l1.next = l2;
+//        l2.next = l3;
+//        l3.next = l4;
+//        l4.next = l5;
+        ListNode res = removeNthFromEnd(l1,1);
         while (res != null) {
             System.out.println(res.val);
             res = res.next;
@@ -21,33 +20,26 @@ public class test {
         int val;
         ListNode next;
 
-        ListNode() {
-        }
+        ListNode() {}
 
-        ListNode(int val) {
-            this.val = val;
-        }
-
-        ListNode(int val, ListNode next) {
-            this.val = val;
-            this.next = next;
-        }
+        ListNode(int x) { val = x; }
     }
 
-    public static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        ListNode prevhead = new ListNode(-1);
-        ListNode prev = prevhead;
-        while (l1 != null && l2 != null) {
-            if (l1.val < l2.val) {
-                prev.next = l1;
-                l1 = l1.next;
-            } else {
-                prev.next = l2;
-                l2 = l2.next;
-            }
-            prev = prev.next;
+    public static ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+        ListNode first = head;
+        int len = 0;
+        while (first != null) {
+            len ++;
+            first = first.next;
         }
-        prev.next = l1 == null ? l2 : l1;
-        return prevhead.next;
+        len -= n;
+        while (len != 0) {
+            len--;
+            dummy = dummy.next;
+        }
+        dummy.next = dummy.next.next;
+        return dummy.next;
     }
 }

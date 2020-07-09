26f674c9ac631b4fba5b3571a3c6f01d5f3ec529
qianqi <qianqi0614@163.com>
2020-07-09 16:13:07 +0800

    Leetcode回文数

--- src/test.java
+++ src/test.java
@@ -1,44 +1,39 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Stack;
-
 public class test {
-    public static class TreeNode {
-        int val;
-        TreeNode left;
-        TreeNode right;
-        public TreeNode (int val) {this.val = val;}
-    }
-
-    public static List<Integer> inorderTraversal(TreeNode root) {
-        List<Integer> list = new ArrayList<>();
-        Stack<TreeNode> stack = new Stack<>();
-        TreeNode curr = root;
-        while (curr != null || !stack.isEmpty()) {
-            while (curr != null) {
-                stack.push(curr);
-                curr = curr.left;
-            }
-            curr = stack.pop();
-            list.add(curr.val);
-            curr = curr.right;
-        }
-        return list;
-    }
-
     public static void main(String[] args) {
-        TreeNode t1 = new TreeNode(1);
-        TreeNode t2 = new TreeNode(2);
-        TreeNode t3 = new TreeNode(3);
-        TreeNode t4 = new TreeNode(4);
-        TreeNode t5 = new TreeNode(5);
-        TreeNode t6 = new TreeNode(6);
-        t1.left = t2;
-        t1.right = t3;
-        t2.left = t4;
-        t2.right = t5;
-        t3.left = t6;
-        List<Integer> list = inorderTraversal(t1);
-        list.forEach(System.out::println);
+        System.out.println(reserve(-134521));
+        System.out.println(isPalindrome(-121));
+    }
+
+    public static boolean isPalindrome(int x) {
+        int temp = x;
+        int res = 0;
+        while (x != 0) {
+            int pop = x % 10;
+            x = x / 10;
+//            if (res > Integer.MAX_VALUE/10 || (res == Integer.MAX_VALUE/10 && pop > Integer.MAX_VALUE%10)) {
+//                return 0;
+//            }
+//            if (res < Integer.MIN_VALUE/10 || (res == Integer.MIN_VALUE/10 && pop < Integer.MIN_VALUE%10)) {
+//                return 0;
+//            }
+            res = res * 10 + pop;
+        }
+        System.out.println(temp);
+        return temp == res;
+    }
+    public static int reserve(int x) {
+        int res = 0;
+        while (x != 0) {
+            int pop = x % 10;
+            x = x / 10;
+            if (res > Integer.MAX_VALUE/10 || (res == Integer.MAX_VALUE/10 && pop > Integer.MAX_VALUE%10)) {
+                return 0;
+            }
+            if (res < Integer.MIN_VALUE/10 || (res == Integer.MIN_VALUE/10 && pop < Integer.MIN_VALUE%10)) {
+                return 0;
+            }
+            res = res * 10 + pop;
+        }
+        return res;
     }
 }

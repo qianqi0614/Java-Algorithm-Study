b99ad0085dc019edbb27b5b2fac1d32bd2dec581
qianqi <qianqi0614@163.com>
2020-07-09 15:09:43 +0800

    Leetcode二叉树的中序遍历非递归版

--- src/test.java
+++ src/test.java
@@ -1,45 +1,44 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
 public class test {
-    public static void main(String[] args) {
-        ListNode l1 = new ListNode(1);
-        ListNode l2 = new ListNode(2);
-        ListNode l3 = new ListNode(3);
-        ListNode l4 = new ListNode(4);
-        ListNode l5 = new ListNode(5);
-        ListNode l6 = new ListNode(6);
-        l1.next = l2;
-        l2.next = l3;
-        l3.next = l4;
-        l4.next = l5;
-//        l5.next = l6;
-        ListNode res = middleNode(l1);
-        while (res != null) {
-            System.out.println(res.val);
-            res = res.next;
-        }
-    }
-
-    public static class ListNode {
+    public static class TreeNode {
         int val;
-        ListNode next;
-
-        ListNode() {}
-
-        ListNode(int x) { val = x; }
+        TreeNode left;
+        TreeNode right;
+        public TreeNode (int val) {this.val = val;}
     }
 
-    public static ListNode middleNode(ListNode head) {
-        ListNode curr = head;
-        int len = 0;
-        while (curr != null) {
-            curr = curr.next;
-            len++;
+    public static List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode curr = root;
+        while (curr != null || !stack.isEmpty()) {
+            while (curr != null) {
+                stack.push(curr);
+                curr = curr.left;
+            }
+            curr = stack.pop();
+            list.add(curr.val);
+            curr = curr.right;
         }
-        len /= 2;
-        curr = head;
-        while (len != 0) {
-            curr = curr.next;
-            len--;
-        }
-        return curr;
+        return list;
+    }
+
+    public static void main(String[] args) {
+        TreeNode t1 = new TreeNode(1);
+        TreeNode t2 = new TreeNode(2);
+        TreeNode t3 = new TreeNode(3);
+        TreeNode t4 = new TreeNode(4);
+        TreeNode t5 = new TreeNode(5);
+        TreeNode t6 = new TreeNode(6);
+        t1.left = t2;
+        t1.right = t3;
+        t2.left = t4;
+        t2.right = t5;
+        t3.left = t6;
+        List<Integer> list = inorderTraversal(t1);
+        list.forEach(System.out::println);
     }
 }

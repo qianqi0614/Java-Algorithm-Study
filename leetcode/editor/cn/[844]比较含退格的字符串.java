886030292eb984b9092835f869a404690d368ae0
qianqi <qianqi0614@163.com>
2020-07-15 16:45:36 +0800

    Leetcode 比较含退格的字符串双指针法

--- leetcode/editor/cn/[844]比较含退格的字符串.java
+++ leetcode/editor/cn/[844]比较含退格的字符串.java
@@ -57,19 +57,56 @@
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public boolean backspaceCompare(String S, String T) {
-        return build(S).equals(build(T));
-    }
+        int i = S.length() - 1, j = T.length() - 1;
+        int skipS = 0, skipT = 0;
 
-    public String build(String str) {
-        Stack<Character> stack = new Stack<>();
-        for (char c : str.toCharArray()) {
-            if (c != '#') {
-                stack.push(c);
-            } else if (!stack.empty()) {
-                stack.pop();
+        while (i >= 0 || j >= 0) {
+            while (i >= 0) {
+                if (S.charAt(i) == '#') {
+                    skipS++;
+                    i--;
+                } else if (skipS > 0) {
+                    skipS--;
+                    i--;
+                } else {
+                    break;
+                }
             }
+            while (j >= 0) {
+                if (T.charAt(j) == '#') {
+                    skipT++;
+                    j--;
+                } else if (skipT > 0) {
+                    skipT--;
+                    j--;
+                } else {
+                    break;
+                }
+            }
+            if (i >= 0 && j >=0 && S.charAt(i) != T.charAt(j)) {
+                return false;
+            }
+            if ((i >= 0) != (j >= 0)) {
+                return false;
+            }
+            i--;j--;
         }
-        return String.valueOf(stack);
+        return true;
     }
+//    public boolean backspaceCompare(String S, String T) {
+//        return build(S).equals(build(T));
+//    }
+//
+//    public String build(String str) {
+//        Stack<Character> stack = new Stack<>();
+//        for (char c : str.toCharArray()) {
+//            if (c != '#') {
+//                stack.push(c);
+//            } else if (!stack.empty()) {
+//                stack.pop();
+//            }
+//        }
+//        return String.valueOf(stack);
+//    }
 }
 //leetcode submit region end(Prohibit modification and deletion)

e619476f9dfd417efa8115670cd3f7fe2a2cc567
qianqi <qianqi0614@163.com>
2020-07-29 15:43:07 +0800

    Leetcode 寻找两个正序数组的中位数

--- src/test.java
+++ src/test.java
@@ -40,19 +40,100 @@ public class test {
 //        }
 //        System.out.print("null");
 
-        System.out.println(lengthOfLongestSubstring("abcdcca"));
+        int[] nums1 = new int[] {3, 4};
+        int[] nums2 = new int[] {1, 2};
+        System.out.println(findMedianSortedArrays(nums1, nums2));
+
     }
 
-    public static int lengthOfLongestSubstring(String s) {
-        int ans = 0, n = s.length();
-        int[] index = new int[128];
-        for (int j = 0, i = 0; j < n; j++) {
-            i = Math.max(i, index[s.charAt(j)]);
-            ans = Math.max(ans, j - i + 1);
-            index[s.charAt(j)] = j + 1;
-        }
-        return ans;
+    public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int n = nums1.length;
+        int m = nums2.length;
+        int left = (n + m + 1) / 2;
+        int right = (n + m + 2) / 2;
+        return (getKth(nums1, 0 , n - 1, nums2, 0, m - 1, left) + getKth(nums1, 0, n - 1, nums2, 0, m - 1, right)) * 0.5;
     }
+
+    public static double getKth(int[] nums1, int start1, int end1, int[] nums2, int start2, int end2, int k) {
+        int len1 = end1 - start1 + 1;
+        int len2 = end2 - start2 + 1;
+        if (len1 > len2) {
+            return getKth(nums2, start2, end2, nums1, start1, end1, k);
+        }
+        if (len1 == 0) {
+            return nums2[start2 + k - 1];
+        }
+
+        if (k == 1) {
+            return Math.min(nums1[start1], nums2[start2]);
+        }
+
+        int i = start1 + Math.min(len1, k / 2) - 1;
+        int j = start2 + Math.min(len2, k / 2) - 1;
+
+        if (nums1[i] > nums2[j]) {
+            return getKth(nums1, start1, end1, nums2, j + 1, end2, k - (j - start2 + 1));
+        } else {
+            return getKth(nums1, i + 1, end1, nums2, start2, end2, k - (i - start1 + 1));
+        }
+    }
+
+//    public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
+//        int m = nums1.length;
+//        int n = nums2.length;
+//        int[] newNums = new int[m + n];
+//        if (m == 0) {
+//            if (n % 2 == 0) {
+//                return (nums2[n / 2] + nums2[n / 2 - 1]) / 2.0;
+//            } else {
+//                return nums2[n / 2];
+//            }
+//        }
+//        if (n == 0) {
+//            if (m % 2 == 0) {
+//                return (nums1[m / 2] + nums1[m / 2 - 1]) / 2.0;
+//            } else {
+//                return nums1[m / 2];
+//            }
+//        }
+//        int count = 0;
+//        int i = 0, j = 0;
+//        while (count != (m + n)) {
+//            if (i == m) {
+//                while (j != n) {
+//                    newNums[count++] = nums2[j++];
+//                }
+//                break;
+//            }
+//            if (j == n) {
+//                while (i != m) {
+//                    newNums[count++] = nums1[i++];
+//                }
+//                break;
+//            }
+//            if (nums1[i] < nums2[j]) {
+//                newNums[count++] = nums1[i++];
+//            } else {
+//                newNums[count++] = nums2[j++];
+//            }
+//        }
+//        if (count % 2 == 0) {
+//            return (newNums[count / 2] + newNums[count / 2 - 1]) / 2.0;
+//        } else {
+//            return newNums[count / 2];
+//        }
+//    }
+
+//    public static int lengthOfLongestSubstring(String s) {
+//        int ans = 0, n = s.length();
+//        int[] index = new int[128];
+//        for (int j = 0, i = 0; j < n; j++) {
+//            i = Math.max(i, index[s.charAt(j)]);
+//            ans = Math.max(ans, j - i + 1);
+//            index[s.charAt(j)] = j + 1;
+//        }
+//        return ans;
+//    }
 
 
 //    public static ListNode reserveBetween(ListNode head, int m, int n) {

b9ddc228f154050fa8984b34b308f08731f016f2
qianqi <qianqi0614@gmail.com>
2020-07-30 15:46:02 +0800

    选择排序练习

---
+++ src/SelectionSort.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+public class SelectionSort {
+    public static void main(String[] args) {
+        int[] nums = {1,6,4,3,7,8,9,5,10,2};
+        System.out.println(Arrays.toString(selectionSort(nums)));
+    }
+
+    public static int[] selectionSort(int[] nums) {
+        for (int i = 0; i < nums.length - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[j] < nums[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            int temp = nums[i];
+            nums[i] = nums[minIndex];
+            nums[minIndex] = temp;
+        }
+        return nums;
+    }
+}

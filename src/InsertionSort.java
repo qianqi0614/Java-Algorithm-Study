c733bc8c23703a1bd347dbfc70246f12ef7efaa2
qianqi <qianqi0614@gmail.com>
2020-07-30 15:08:06 +0800

    插入排序练习

---
+++ src/InsertionSort.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+public class InsertionSort {
+    public static void main(String[] args) {
+        int[] nums = {5,4,3,1,6,7,2,9,8,10};
+        System.out.println(Arrays.toString(insertionSort(nums)));
+    }
+
+    public static int[] insertionSort(int[] nums) {
+        for (int i = 1; i < nums.length; i++) {
+            int value = nums[i];
+            int j = i -1;
+            for (; j >= 0; j--) {
+                if (value < nums[j]) {
+                    nums[j + 1] = nums[j];
+                } else {
+                    break;
+                }
+            }
+            nums[j + 1] = value;
+        }
+        return nums;
+    }
+}

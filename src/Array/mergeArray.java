b5648b1ecd29e3c2dfa53388cba00f086c13c180
qianqi <qianqi0614@163.com>
2020-10-28 14:26:59 +0800

    合并两个数组

--- src/Array/mergeArray.java
+++ src/Array/mergeArray.java
@@ -1,4 +1,29 @@
-package PACKAGE_NAME;
+package Array;
+
+import java.util.Arrays;
 
 public class mergeArray {
+
+
+    public static void main(String[] args) {
+        int m = 4;
+        int n = 3;
+        int[] array1 = {2, 3, 4, 5};
+        int[] array2 = {9, 8, 1};
+        int[] newArray = merge(array1,m,array2,n);
+        System.out.println(Arrays.toString(newArray));
+    }
+
+    public static int[] merge(int[] array1, int m, int[] array2, int n) {
+        int[] newArray = new int[m + n];
+        int i = m - 1;
+        int j = 0;
+        int k = m + n - 1;
+        while (i >= 0 && j < n) {
+            newArray[k--] = array1[i] > array2[j] ? array1[i--] : array2[j++];
+        }
+        newArray[0] = Math.min(array1[0], array2[n-1]);
+        return newArray;
+    }
+
 }
--- src/demo.java
+++ src/demo.java
@@ -1,4 +1,36 @@
-package PACKAGE_NAME;
+import java.util.*;
 
 public class demo {
+    public static void main(String[] args) {
+        HashMap<String,Integer> map = new HashMap<>(100);
+        map.put("aa", 10);
+        map.put("bb", 11);
+        map.put("cc", 12);
+        int maxValue = Integer.parseInt(Objects.requireNonNull(findMaxValue(map)).toString());
+        String maxKey = findMaxKey(map, maxValue);
+        System.out.println(maxKey+"->"+maxValue);
+        map.remove(maxKey,maxValue);
+        System.out.println("----------------");
+        map.forEach((k,v) -> System.out.println(k + "->" + v));
+    }
+
+    private static String findMaxKey(HashMap<String, Integer> map, int maxValue) {
+        String key = "";
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            if (maxValue == entry.getValue()) {
+                key = entry.getKey();
+            }
+        }
+        return key;
+    }
+
+    private static Object findMaxValue(HashMap<String, Integer> map) {
+        if (map.size() == 0) {
+            return null;
+        }
+        Collection<Integer> c = map.values();
+        Object[] obj = c.toArray();
+        Arrays.sort(obj);
+        return obj[obj.length-1];
+    }
 }

35f447e6157d99970f8f9d8868fb36fea47ae74b
qianqi <qianqi0614@163.com>
2020-10-29 17:36:38 +0800

    面试题

---
+++ leetcode/editor/cn/[面试题 01.04]回文排列.java
@@ -0,0 +1,26 @@
+//给定一个字符串，编写一个函数判定其是否为某个回文串的排列之一。 
+//
+// 回文串是指正反两个方向都一样的单词或短语。排列是指字母的重新排列。 
+//
+// 回文串不一定是字典当中的单词。 
+//
+// 
+//
+// 示例1： 
+//
+// 输入："tactcoa"
+//输出：true（排列有"tacocat"、"atcocta"，等等）
+// 
+//
+// 
+// Related Topics 哈希表 字符串 
+// 👍 35 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public boolean canPermutePalindrome(String s) {
+
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)

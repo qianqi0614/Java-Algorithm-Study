38180deddcb94b385d1f6fb933a07eb5fced9afa
qianqi <qianqi0614@gmail.com>
2021-05-09 03:53:31 +0800

    test2

--- src/demo.java
+++ src/demo.java
@@ -34,7 +34,7 @@ public class demo {
         return obj[obj.length-1];
     }
 
-    public static void test12() {
+    public static void test1112() {
         System.out.println("hello world!");
     }
 }
